314f4375ca47c422b8f9c363fd237f4d0fdc3ed5
UncleGeorge <george.cosmoswill@gmail.com>
2023-07-18 16:13:45 -0400

    finished account view

--- MyLessee_iOS_SwiftUI/AccountView.swift
+++ MyLessee_iOS_SwiftUI/AccountView.swift
@@ -7,9 +7,41 @@
 
 import SwiftUI
 
+struct AccountSection: Identifiable {
+    let id = UUID()
+    let iconName: String
+    let text: String
+}
+
 struct AccountView: View {
+    
+    let items: [AccountSection] = [
+        AccountSection(iconName: "ProfileOutline", text: "Profile"),
+        AccountSection(iconName: "ListOutLine", text: "My Listings"),
+        AccountSection(iconName: "BookmarkOutline", text: "Bookmarks"),
+        AccountSection(iconName: "DiamondOutline", text: "Premium Points"),
+        AccountSection(iconName: "SettingsOutline", text: "Settings")
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Alvaro Beltran")
+                .font(.title)
+            Text("Posts : 124 | Listings : 0")
+                .foregroundColor(Color.gray)
+            List(items) { item in
+                HStack {
+                    Image(item.iconName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 35, height: 35)
+                    Text(item.text)
+                }
+                .padding(2)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(PlainListStyle())
+        }
     }
 }
 
