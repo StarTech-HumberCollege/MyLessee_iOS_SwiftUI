eba37faf1c13e080c851c2e1519abf6cb4e99f83
Tuyi <tuyi.chen90@gmail.com>
2023-07-18 16:10:39 -0400

    Create HeaderView and upload logo images

---
+++ MyLessee_iOS_SwiftUI/HeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  HeaderView.swift
+//  MyLessee_iOS_SwiftUI
+//
+//  Created by Tuyi Chen on 2023-07-18.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    
+    let header: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image("alvaro")
+                    .resizable().frame(width: 50, height: 50)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                Spacer()
+                Image("logo")
+                    .resizable().frame(width: 180, height: 40)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 5))
+            }
+            Text(header).bold().font(.title)
+                .foregroundColor(Color(red: 0xE3 / 255, green: 0x6F / 255, blue: 0x5B / 255))
+        }
+        .frame(width: UIScreen.main.bounds.width)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView(header: "Header")
+    }
+}
