3a12cc8b88aab7adf7bd4d3a07517b1fb2222b50
UncleGeorge <george.cosmoswill@gmail.com>
2023-07-18 14:40:23 -0400

    first commit

---
+++ MyLessee_iOS_SwiftUI/MyLessee_iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLessee_iOS_SwiftUIApp.swift
+//  MyLessee_iOS_SwiftUI
+//
+//  Created by Zhangzhi Wang on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct MyLessee_iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
