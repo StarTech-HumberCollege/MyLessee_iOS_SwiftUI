d05d019c43416481210c71f8641a181f29399e92
Tuyi <tuyi.chen90@gmail.com>
2023-07-18 23:24:23 -0400

    create Home group

---
+++ MyLessee_iOS_SwiftUI/Home/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  MyLessee_iOS_SwiftUI
+//
+//  Created by Tuyi Chen on 2023-07-18.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
